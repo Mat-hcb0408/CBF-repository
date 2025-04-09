71897b4d3d3dcf50bdbe8bf2c109ddc8edb55595
Thallys_San <ricardo.viasolo142@gmail.com>
2025-04-09 15:32:30 -0300

    Update conection.java

--- conection.java
+++ conection.java
@@ -2,21 +2,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class conection {
-    public static void main(String[] args) throws SQLException {
-// Defina as informações de conexão
-        String url = "jdbc:mysql://localhost:3306"; // URL de conexão com o
-        String user = "root"; // Nome do usuário
-        String password = null; // Senha do banco de dados
-// Conectar ao banco de dados
-        try (Connection conexao = DriverManager.getConnection(url, user, null))
-        {
+public class Conexao {
 
-            System.out.println("Conexão realizada com sucesso!");
+    private static final String URL = "jdbc:mysql://localhost:3306/tarde_bd_cbf_teste";
+    private static final String USER = "root";
+    private static final String PASSWORD = null; // ou "" se for senha vazia
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
