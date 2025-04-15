84a525ed57098274a7f0eab986e1c7061d2b6326
David-son-frs <fdavidson991@gmail.com>
2025-04-15 16:46:30 -0300

    Create Controlador.java

---
+++ front-end/Controlador.java
@@ -0,0 +1,40 @@
+package org.example.projetinho;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
+
+public class HelloController {
+
+    @FXML private TableView<?> tabelaClassificacao;
+    @FXML private TableColumn<?, ?> colPosicao;
+    @FXML private TableColumn<?, ?> colTime;
+    @FXML private TableColumn<?, ?> colPontos;
+    @FXML private TableColumn<?, ?> colJogos;
+    @FXML private TableColumn<?, ?> colVitorias;
+    @FXML private TableColumn<?, ?> colEmpates;
+    @FXML private TableColumn<?, ?> colDerrotas;
+    @FXML private TableColumn<?, ?> colGolsPro;
+    @FXML private TableColumn<?, ?> colGolsContra;
+    @FXML private TableColumn<?, ?> colSaldoGols;
+    @FXML private TableColumn<?, ?> colAproveitamento;
+
+    @FXML
+    public void initialize() {
+        configureTable();
+    }
+
+    private void configureTable() {
+        tabelaClassificacao.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
+
+    @FXML
+    private void handleAtualizar() {
+        // Empty refresh method
+        tabelaClassificacao.refresh();
+    }
+}
