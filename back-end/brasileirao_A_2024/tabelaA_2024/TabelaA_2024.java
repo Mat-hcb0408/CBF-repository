704c77b2ae70c186d3178f0763f658947084128e
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-04-17 03:08:18 -0300

    Add files via upload

---
+++ back-end/brasileirao_A_2024/tabelaA_2024/TabelaA_2024.java
@@ -0,0 +1,61 @@
+package com.cbf1.cbf1.tabelaA_2024;
+
+public class TabelaA_2024 {
+    protected final Integer id_clube_A24;
+    protected final Integer vitorias_A24;
+    protected final Integer empates_A24;
+    protected final Integer derrotas_A24;
+    protected final Integer golsPro_A24;
+    protected final Integer golsContra_A24;
+    protected final Integer jogos_A24;
+    protected final Integer pontos_A24;
+    protected final Integer saldoGols_A24;
+
+    public TabelaA_2024(Integer id_clube_A24, Integer vitorias_A24, Integer empates_A24, Integer derrotas_A24, Integer golsPro_A24, Integer golsContra_A24, Integer jogos_A24, Integer pontos_A24, Integer saldoGols_A24) {
+        this.id_clube_A24 = id_clube_A24;
+        this.vitorias_A24 = vitorias_A24;
+        this.empates_A24 = empates_A24;
+        this.derrotas_A24 = derrotas_A24;
+        this.golsPro_A24 = golsPro_A24;
+        this.golsContra_A24 = golsContra_A24;
+        this.jogos_A24 = jogos_A24;
+        this.pontos_A24 = pontos_A24;
+        this.saldoGols_A24 = saldoGols_A24;
+    }
+
+    public Integer getId_clube_A24() {
+        return id_clube_A24;
+    }
+
+    public Integer getVitorias_A24() {
+        return vitorias_A24;
+    }
+
+    public Integer getEmpates_A24() {
+        return empates_A24;
+    }
+
+    public Integer getDerrotas_A24() {
+        return derrotas_A24;
+    }
+
+    public Integer getGolsPro_A24() {
+        return golsPro_A24;
+    }
+
+    public Integer getGolsContra_A24() {
+        return golsContra_A24;
+    }
+
+    public Integer getJogos_A24() {
+        return jogos_A24;
+    }
+
+    public Integer getPontos_A24() {
+        return pontos_A24;
+    }
+
+    public Integer getSaldoGols_A24() {
+        return saldoGols_A24;
+    }
+}
