de0b22e5a0f0d7e8f1a9381c9f2ff46ee7d48818
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-04-17 03:55:07 -0300

    Add files via upload

---
+++ back-end/brasileirao_b_2024/tabelaB_2024/TabelaB_2024.java
@@ -0,0 +1,61 @@
+package com.cbf1.cbf1.tabelaB_2024;
+
+public class TabelaB_2024 {
+    protected final Integer id_clube_B24;
+    protected final Integer vitorias_B24;
+    protected final Integer empates_B24;
+    protected final Integer derrotas_B24;
+    protected final Integer golsPro_B24;
+    protected final Integer golsContra_B24;
+    protected final Integer jogos_B24;
+    protected final Integer pontos_B24;
+    protected final Integer saldoGols_B24;
+
+    public TabelaB_2024(Integer id_clube_B24, Integer vitorias_B24, Integer empates_B24, Integer derrotas_B24, Integer golsPro_B24, Integer golsContra_B24, Integer jogos_B24, Integer pontos_B24, Integer saldoGols_B24) {
+        this.id_clube_B24 = id_clube_B24;
+        this.vitorias_B24 = vitorias_B24;
+        this.empates_B24 = empates_B24;
+        this.derrotas_B24 = derrotas_B24;
+        this.golsPro_B24 = golsPro_B24;
+        this.golsContra_B24 = golsContra_B24;
+        this.jogos_B24 = jogos_B24;
+        this.pontos_B24 = pontos_B24;
+        this.saldoGols_B24 = saldoGols_B24;
+    }
+
+    public Integer getId_clube_B24() {
+        return id_clube_B24;
+    }
+
+    public Integer getVitorias_B24() {
+        return vitorias_B24;
+    }
+
+    public Integer getEmpates_B24() {
+        return empates_B24;
+    }
+
+    public Integer getDerrotas_B24() {
+        return derrotas_B24;
+    }
+
+    public Integer getGolsPro_B24() {
+        return golsPro_B24;
+    }
+
+    public Integer getGolsContra_B24() {
+        return golsContra_B24;
+    }
+
+    public Integer getJogos_B24() {
+        return jogos_B24;
+    }
+
+    public Integer getPontos_B24() {
+        return pontos_B24;
+    }
+
+    public Integer getSaldoGols_B24() {
+        return saldoGols_B24;
+    }
+}
---
+++ back-end/brasileirao_b_2024/tabelaB_2024/TabelaB_2024Controller.java
@@ -0,0 +1,86 @@
+package com.cbf1.cbf1.tabelaB_2024;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.*;
+
+public class TabelaB_2024Controller {
+
+
+    @FXML
+    private TableView<TabelaB_2024> tabelaB2024;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colIdClube;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colVitorias;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colEmpates;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colDerrotas;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colGolsPro;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colGolsContra;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colJogos;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colPontos;
+    @FXML
+    private TableColumn<TabelaB_2024, Integer> colSaldoGols;
+
+
+    private ObservableList<TabelaB_2024> listaTabelaB_2024;
+
+    public void initialize() {
+
+        this.colIdClube.setCellValueFactory(new PropertyValueFactory<>("id_clube_B24"));
+        this.colVitorias.setCellValueFactory(new PropertyValueFactory<>("vitorias_B24"));
+        this.colEmpates.setCellValueFactory(new PropertyValueFactory<>("empates_B24"));
+        this.colDerrotas.setCellValueFactory(new PropertyValueFactory<>("derrotas_B24"));
+        this.colGolsPro.setCellValueFactory(new PropertyValueFactory<>("golsPro_B24"));
+        this.colGolsContra.setCellValueFactory(new PropertyValueFactory<>("golsContra_B24"));
+        this.colJogos.setCellValueFactory(new PropertyValueFactory<>("jogos_B24"));
+        this.colPontos.setCellValueFactory(new PropertyValueFactory<>("pontos_B24"));
+        this.colSaldoGols.setCellValueFactory(new PropertyValueFactory<>("saldoGols_B24"));
+
+        loadFromDatabase();
+    }
+
+    private void loadFromDatabase() {
+        this.listaTabelaB_2024 = FXCollections.observableArrayList();
+        String url="jdbc:mysql://localhost:3306/tarde_bd_cbf_teste";
+        String user="root";
+        String password=null;
+
+        String query="select * from brasileirao_serieb_2024";
+
+        try (
+                Connection conn=DriverManager.getConnection(url,user,password);
+                PreparedStatement stmt = conn.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery();
+        ){while (rs.next()){
+            TabelaB_2024 b24=new TabelaB_2024(
+                    rs.getInt("idClube"),
+                    rs.getInt("vitorias"),
+                    rs.getInt("empates"),
+                    rs.getInt("derrotas"),
+                    rs.getInt("golsPro"),
+                    rs.getInt("golsSofridos"),
+                    rs.getInt("jogos"),
+                    rs.getInt("pontos"),
+                    rs.getInt("saldoGols")
+            );
+            this.listaTabelaB_2024.add(b24);
+        }
+            this.tabelaB2024.setItems(this.listaTabelaB_2024);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+}
