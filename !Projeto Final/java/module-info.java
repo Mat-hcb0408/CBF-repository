d87b130cf4c9122cd99b7250efd07c9a4a0d5853
Thallys_San <ricardo.viasolo142@gmail.com>
2025-04-17 02:23:30 -0300

    Update module-info.java

--- !Projeto Final/java/module-info.java
+++ !Projeto Final/java/module-info.java
@@ -6,14 +6,15 @@ module org.cbf.cbf_teste {
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
 
-    opens org.cbf.cbf to javafx.fxml;
     exports org.cbf.cbf.repository;
     opens org.cbf.cbf.repository to javafx.fxml;
+
     exports org.cbf.cbf.controller;
     opens org.cbf.cbf.controller to javafx.fxml;
+
     exports org.cbf.cbf.model;
     opens org.cbf.cbf.model to javafx.fxml;
+
     exports org.cbf.cbf.main;
     opens org.cbf.cbf.main to javafx.fxml;
-
 }
