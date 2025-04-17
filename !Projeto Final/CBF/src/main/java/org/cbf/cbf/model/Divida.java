397c46f6632b78c6095fe775e2656b2781e9bb98
Thallys_San <ricardo.viasolo142@gmail.com>
2025-04-17 02:51:39 -0300

    Add files via upload

---
+++ !Projeto Final/CBF/src/main/java/org/cbf/cbf/model/Divida.java
@@ -0,0 +1,34 @@
+package org.cbf.cbf.model;
+
+public class Divida {
+    private Integer id;
+    private Double valor;
+    private String validade;
+    private Double juros;
+    private String credor;
+    private String fiador;
+    private String tipo;
+    private String descricao;
+
+    // Construtor, getters e setters
+    public Divida(Integer id, Double valor, String validade, Double juros, String credor,String fiador, String tipo, String descricao) {
+        this.id = id;
+        this.valor = valor;
+        this.validade = validade;
+        this.juros = juros;
+        this.credor = credor;
+        this.fiador = fiador;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    // Getters
+    public Integer getId() { return id; }
+    public Double getValor() { return valor; }
+    public String getValidade() { return validade; }
+    public Double getJuros() { return juros; }
+    public String getCredor() { return credor; }
+    public String getFiador() {return fiador; }
+    public String getTipo() { return tipo; }
+    public String getDescricao() { return descricao; }
+}
